78cac8d362fe5cebf70400951df405986ddd73fa
ercclpn <ercclpn@gmail.com>
2019-03-11 08:52:10 +0100

    Remove the duplicate ; in the FileTransformer.java .

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -61,7 +61,7 @@ public abstract class FileTransformer implements IFileVisitor {
       int c = reader.read();
 
       while(c != -1){
-        writer.write(c);;
+        writer.write(c);
         c = reader.read();
       }
       
